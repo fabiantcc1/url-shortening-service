6ef22a119982f7a8137b23c9df32104c8d39ecdb
Fabian Montaño <fabiantccfa@gmail.com>
2025-03-21 22:36:59 -0600

    add: implement POST route for URL shortening with unique code generation

--- routes/shorten.route.js
+++ routes/shorten.route.js
@@ -1,9 +1,22 @@
+import { config } from '../config/config.js';
 import express from 'express';
+import { nanoid } from 'nanoid';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Main shorten' });
+router.post('/', (req, res) => {
+    const body = req.body;
+
+    const urlCode = nanoid(8);
+    const shortUrl = `${config.url}/${urlCode}`;
+
+    res.json({
+        id: 1,
+        url: shortUrl,
+        shortCode: urlCode,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
 });
 
 export default router;
