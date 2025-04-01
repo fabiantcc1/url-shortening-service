e99acec133e68cb618a51118dcff197e7b7abbb4
Fabian Montaño <fabiantccfa@gmail.com>
2025-03-31 21:46:55 -0600

    refactor: enhance POST route for URL shortening with error handling

--- routes/shorten.route.js
+++ routes/shorten.route.js
@@ -4,19 +4,23 @@ import { nanoid } from 'nanoid';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    const { url } = req.body;
+router.post('/', (req, res, next) => {
+    try {
+        const { url } = req.body;
 
-    const urlCode = nanoid(8);
-    const shortUrl = `${config.url}/${urlCode}`;
+        const urlCode = nanoid(8);
+        const shortUrl = `${config.url}/${urlCode}`;
 
-    res.json({
-        id: 1,
-        url: url,
-        shortCode: urlCode,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    });
+        res.json({
+            id: 1,
+            url: url,
+            shortCode: urlCode,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default router;
