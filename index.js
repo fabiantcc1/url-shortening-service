81433ad8857ac3b2779e3c555557d8bfaf0e5332
Fabian Montaño <fabiantccfa@gmail.com>
2025-03-31 21:45:45 -0600

    refactor: integrate configuration and error handling middleware into the Express server setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import routerApi from './routes/index.js';
+import { config } from './config/config.js';
+import { logError } from './middleware/error.handler.js';
+import { errorHandler } from './middleware/error.handler.js';
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json());
+
 routerApi(app);
 
+app.use(logError);
+app.use(errorHandler);
+
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    if (config.env === 'development') {
+        console.log(`Server is running at http://localhost:${port}`);
+    } else {
+        console.log(`Server is running`);
+    }
 });
