39b6f17736a985a1303e6930148dd5e549b7d712
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-04 21:52:29 -0600

    refactor: integrate validation middleware into URL shortening route
    - Added validateHandler to validate request body using createShortUrlSchema
    - Improved error handling structure in the POST route for URL shortening

--- routes/shorten.route.js
+++ routes/shorten.route.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import { ShortenService } from '../services/shorten.service.js';
+import { validateHandler } from '../middleware/validator.handler.js';
+import { createShortUrlSchema } from '../schemas/url.scheme.js';
 
 const router = express.Router();
 const service = new ShortenService();
 
-router.post('/', async (req, res, next) => {
-    try {
-        const { url } = req.body;
-        const shortUrl = await service.create(url);
+router.post(
+    '/',
+    validateHandler(createShortUrlSchema, 'body'),
+    async (req, res, next) => {
+        try {
+            const { url } = req.body;
+            const shortUrl = await service.create(url);
 
-        res.status(201).json(shortUrl);
-    } catch (error) {
-        next(error);
-    }
-});
+            res.status(201).json(shortUrl);
+        } catch (err) {
+            next(err);
+        }
+    },
+);
 
 export default router;
