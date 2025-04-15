369bd3063aa5bfb9253b7a4b6b48755ff78e4d27
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-14 21:14:14 -0600

    feat: add database URI configuration to the application
    - Introduced a new URI setting in the database configuration to support connection via environment variable.

--- config/config.js
+++ config/config.js
@@ -12,5 +12,6 @@ export const config = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
+        uri: process.env.DB_URI,
     },
 };
