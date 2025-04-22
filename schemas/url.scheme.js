c978f26ab8f675761f1315709f7b70670667e874
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-21 22:00:39 -0600

    refactor: update URL schema to include URI validation for createShortUrlSchema

--- schemas/url.scheme.js
+++ schemas/url.scheme.js
@@ -1,10 +1,22 @@
 import Joi from 'joi';
 import { config } from '.././config/config.js';
 
-const url = Joi.string().required();
+const url = Joi.string()
+    .required()
+    .uri({
+        scheme: config.env === 'development' ? ['http', 'https'] : ['https'],
+    });
+const shortCode = Joi.string().required().min(8).max(8);
 
 export const createShortUrlSchema = Joi.object({
-    url: url.uri({
-        scheme: config.env === 'development' ? ['http', 'https'] : ['https'],
-    }),
+    url,
+});
+
+export const getShortUrlSchema = Joi.object({
+    shortCode,
+});
+
+export const updateShortUrlSchema = Joi.object({
+    shortCode,
+    url,
 });
