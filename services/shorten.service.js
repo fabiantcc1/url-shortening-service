25274404bee74ba96a117727f403687a4f12fdeb
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-21 22:00:17 -0600

    update: implement findByShortCode and update methods in ShortenService

--- services/shorten.service.js
+++ services/shorten.service.js
@@ -1,6 +1,7 @@
 import { config } from '../config/config.js';
 import { nanoid } from 'nanoid';
 import { sequelize } from '../libs/sequelize.js';
+import Boom from '@hapi/boom';
 
 export class ShortenService {
     constructor() {
@@ -20,5 +21,43 @@ export class ShortenService {
         shortUrl.dataValues.fullShortUrl = `${config.url}/${urlCode}`;
 
         return shortUrl;
+    }
+
+    async findByShortCode(shortCode) {
+        const url = await this.models.Url.findOne({
+            where: {
+                shortCode,
+                isActive: true,
+            },
+            attributes: { exclude: ['isActive'] },
+        });
+
+        if (!url) {
+            throw Boom.notFound('The URL with this short code does not exist');
+        }
+
+        return url;
+    }
+
+    async update(shortCode, newUrl) {
+        const url = await this.models.Url.findOne({
+            where: {
+                shortCode,
+                isActive: true,
+            },
+        });
+
+        if (!url) {
+            throw Boom.notFound('The URL with this short code does not exist');
+        }
+
+        const updatedUrl = await url.update({
+            originalUrl: newUrl,
+        });
+
+        delete updatedUrl.dataValues.isActive;
+        updatedUrl.dataValues.fullShortUrl = `${config.url}/${shortCode}`;
+
+        return updatedUrl;
     }
 }
