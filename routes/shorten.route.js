d38b5cd01658f3072110301794d035d73b6008d9
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-21 22:00:56 -0600

    update: add GET and PATCH routes for retrieving and updating URLs

--- routes/shorten.route.js
+++ routes/shorten.route.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { ShortenService } from '../services/shorten.service.js';
 import { validateHandler } from '../middleware/validator.handler.js';
-import { createShortUrlSchema } from '../schemas/url.scheme.js';
+import {
+    createShortUrlSchema,
+    getShortUrlSchema,
+    updateShortUrlSchema,
+} from '../schemas/url.scheme.js';
 
 const router = express.Router();
 const service = new ShortenService();
@@ -15,6 +19,38 @@ router.post(
             const shortUrl = await service.create(url);
 
             res.status(201).json(shortUrl);
+        } catch (err) {
+            next(err);
+        }
+    },
+);
+
+router.get(
+    '/:shortCode',
+    validateHandler(getShortUrlSchema, 'params'),
+    async (req, res, next) => {
+        try {
+            const { shortCode } = req.params;
+            const url = await service.findByShortCode(shortCode);
+
+            res.status(200).json(url);
+        } catch (err) {
+            next(err);
+        }
+    },
+);
+
+router.patch(
+    '/:shortCode',
+    validateHandler(getShortUrlSchema, 'params'),
+    validateHandler(createShortUrlSchema, 'body'),
+    async (req, res, next) => {
+        try {
+            const { shortCode } = req.params;
+            const { url } = req.body;
+            const updatedUrl = await service.update(shortCode, url);
+
+            res.status(200).json(updatedUrl);
         } catch (err) {
             next(err);
         }
