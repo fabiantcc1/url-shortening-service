944e67ab715149012746bf94e6380b078a4c3e46
Fabian Montaño <fabiantccfa@gmail.com>
2025-04-24 21:06:08 -0600

    feat: add redirect router to handle catch-all redirects

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import shortenRouter from './shorten.route.js';
+import redirectRouter from './redirect.route.js';
 
 function routerApi(app) {
     const routerV1 = express.Router();
 
     app.use('/api/v1', routerV1);
     routerV1.use('/shorten', shortenRouter);
+
+    // Route catch-all for redirects
+    app.use('/', redirectRouter);
 }
 
 export default routerApi;
